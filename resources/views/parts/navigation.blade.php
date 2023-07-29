7d510692651e24895749ec07ed920106fae9dab2
utof0x <popovychmykhailo00@gmail.com>
2023-07-29 21:25:45 +0300

    fixed links for authenticated users and not

--- resources/views/parts/navigation.blade.php
+++ resources/views/parts/navigation.blade.php
@@ -4,12 +4,12 @@
             <li class="mr-4"><a href="/">Articles</a></li>
             <li class="mr-4"><a href="/about">About</a></li>
             @if(Auth::guest())
+                <li class="ml-auto"><a href="/login">Login</a></li>
+            @else
                 <li class="mr-4 ml-auto"><a href="/categories">Categories</a></li>
                 <li class="mr-4"><a href="/tags">Tags</a></li>
                 <li class="mr-4"><a href="/posts">Posts</a></li>
                 <li><a href="/logout">Logout</a></li>
-            @else
-                <li class="ml-auto"><a href="/login">Login</a></li>
             @endif
         </ul>
     </nav>
