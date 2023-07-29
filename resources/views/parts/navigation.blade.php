018316d1512918a1d03837e3cbbf6cb28e7e8677
utof0x <popovychmykhailo00@gmail.com>
2023-07-29 18:50:52 +0300

    added breeze template for login, home page and navigation

---
+++ resources/views/parts/navigation.blade.php
@@ -0,0 +1,16 @@
+<header>
+    <nav>
+        <ul class="flex p-4">
+            <li class="mr-4"><a href="/">Articles</a></li>
+            <li class="mr-4"><a href="/about">Articles</a></li>
+            @if(Auth::guest())
+                <li class="mr-4 ml-auto"><a href="/categories">Categories</a></li>
+                <li class="mr-4"><a href="/tags">Tags</a></li>
+                <li class="mr-4"><a href="/posts">Posts</a></li>
+                <li><a href="/logout">Logout</a></li>
+            @else
+                <li class="ml-auto"><a href="/login">Login</a></li>
+            @endif
+        </ul>
+    </nav>
+</header>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'pages.home');
+
+require __DIR__.'/auth.php';
