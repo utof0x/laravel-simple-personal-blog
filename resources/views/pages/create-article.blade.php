ef75023c6974159d97f12a70f9ec9fd94fd3205c
utof0x <popovychmykhailo00@gmail.com>
2023-07-30 16:39:39 +0300

    added resource route and controller for articles

---
+++ resources/views/pages/create-article.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base')
+
+@section('content')
+    <div>Here will be create article page</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ProfileController;
 use App\Models\Article;
 use Illuminate\Support\Facades\Route;
@@ -15,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'pages.home', ['articles' => Article::all()]);
+Route::get('/', [ArticleController::class, 'index']);
 Route::view('/about', 'pages.about');
+
+Route::resource('articles', ArticleController::class);
+
 
 require __DIR__.'/auth.php';
